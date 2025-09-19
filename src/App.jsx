4c6e7a157ebc101434e14f75b70f68d0a782e9d8
Alwi Kasim <alwikasim321@gmail.com>
2025-09-19 14:28:00 +0800

    + router, tanstack initial config

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import './App.css';
-import { useQuery } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Login from './pages/Login';
 
 function App() {
-  const { data } = useQuery({
-    queryKey: ['spms'],
-    queryFn: fetchSPM,
-  });
-
-  async function fetchSPM() {
-    try {
-      const res = await fetch('http://localhost:3000/api/kode-akun/1/flags', {
-        method: 'GET',
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6Ik9wZXJhdG9yIEJQUyBLYWIuIEdvcm9udGFsbyIsInJvbGUiOiJvcF9zYXRrZXIiLCJzYXRrZXJJZCI6MiwiaWF0IjoxNzU4MTc5MDE1LCJleHAiOjE3NTgyNjU0MTV9.IFusmbm0p6T2l8T2YqlpYwuoaJgskQVcfBqukHsllQA',
-        },
-      });
-      return await res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <>
-      <p>{JSON.stringify(data)}</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <div className="bg-red-200">Test</div>
+    </>
+  );
+}
+
+export default Login;
